2558e6a77478a8ec94898e8472793166e6db0239
Nahedson <nahedson@gmail.com>
2017-07-10 11:50:00 -0300

    média, nome, ano (falta lista de anos e quantidade de municipios atendidos)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,72 @@ import {SamuService} from './services/samu.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
+  providers:[UFService,SamuService]
 })
 export class AppComponent implements OnInit {
     title = 'app';
+    cabecalho = '';
+    uf_id = '';
+    area= '';
+    mediaAtendidos = 0;
+    anoQtdmunicipios: number[];
     ufs : UF[];
     dados_da_samu : Dados[];
 
     constructor(private ufService: UFService, private samuService: SamuService)
-    { }
+    {
+
+    }
 
     ngOnInit(): void {
         this.ufs = this.ufService.getAll();
         this.dados_da_samu = this.samuService.getAllMunicipiosAtendidosPorEstado();
+        this.usarTitulo();
+        this.criarDetail();
+        this.criarMaster();
     }
+
+    usarTitulo(): void{
+      for(let uf of this.ufs){
+        if (uf.id == 29) this.title = uf.nome;
+      }
+
+    }
+
+    criarDetail(): void
+    {
+      for(let uf of this.ufs)
+      {
+        if(uf.id == 29)
+        {
+          this.uf_id = String(uf.id);
+          this.area = String(uf.area);
+
+        }
+        let i= 0
+        for(let Dados of this.dados_da_samu)
+        {
+            if(Dados.uf_id==29)
+            {
+              this.mediaAtendidos += Dados.valor;
+              i++;
+            }
+        }
+        this.mediaAtendidos = this.mediaAtendidos/i;
+      }
+
+
+    }
+    criarMaster(): void
+    {
+        for(let Dados of this.dados_da_samu)
+        {
+          if(Dados.uf_id==29) this.anoQtdmunicipios.push(Dados);
+        }
+
+    }
+
+
 }
